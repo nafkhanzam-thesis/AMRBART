fde13b74df8cd2d06ec239a1d634bf0e77f5ca94
Moch. Nafkhan Alzamzami <nafkhanalzamzami@gmail.com>
2023-01-07 00:23:04 +0700

    add byte encoder

--- mt5-pre-train/model_interface/tokenization.py
+++ mt5-pre-train/model_interface/tokenization.py
@@ -1,13 +1,14 @@
 # coding:utf-8
 # this is a simplified version of "https://github.com/SapienzaNLP/spring/blob/main/spring_amr/tokenization_bart.py"
-import sys
-import penman
 import json
+import sys
+
+import penman
 import regex as re
-from transformers import T5Tokenizer as TargetTokenizer
 from common import postprocessing
-from common.penman_interface import encode
 from common.constant import raw_special_tokens, recategorizations
+from common.penman_interface import encode
+from transformers import T5Tokenizer as TargetTokenizer
 
 
 class AMRTokenizer(TargetTokenizer):
@@ -40,6 +41,8 @@ class AMRTokenizer(TargetTokenizer):
         self.remove_pars = False
 
         #~ Compatibilities from AMRBartTokenizer
+        self.byte_encoder = bytes_to_unicode()
+        self.byte_decoder = {v: k for k, v in self.byte_encoder.items()}
         self.encoder = {}
         self.decoder = {v: k for k, v in self.encoder.items()}
 
@@ -55,9 +58,8 @@ class AMRTokenizer(TargetTokenizer):
         tokens = [t for t in raw_special_tokens if t not in vocab]
         super()._add_tokens(tokens)
         self.modified = len(tokens)
-        #~
-
         self.encoder = super().get_vocab()
+        #~
 
         self.amr_bos_token = "<AMR>"
         self.amr_bos_token_id = self.encoder[self.amr_bos_token]
@@ -478,3 +480,31 @@ class AMRTokenizer(TargetTokenizer):
             return "INST"
         else:
             return 'CONST'
+
+
+from functools import lru_cache
+
+
+@lru_cache()
+def bytes_to_unicode():
+    """
+    Returns list of utf-8 byte and a mapping to unicode strings. We specifically avoids mapping to whitespace/control
+    characters the bpe code barfs on.
+
+    The reversible bpe codes work on unicode strings. This means you need a large # of unicode characters in your vocab
+    if you want to avoid UNKs. When you're at something like a 10B token dataset you end up needing around 5K for
+    decent coverage. This is a significant percentage of your normal, say, 32K bpe vocab. To avoid that, we want lookup
+    tables between utf-8 bytes and unicode strings.
+    """
+    bs = (
+        list(range(ord("!"), ord("~") + 1)) + list(range(ord("¡"), ord("¬") + 1)) + list(range(ord("®"), ord("ÿ") + 1))
+    )
+    cs = bs[:]
+    n = 0
+    for b in range(2**8):
+        if b not in bs:
+            bs.append(b)
+            cs.append(2**8 + n)
+            n += 1
+    cs = [chr(n) for n in cs]
+    return dict(zip(bs, cs))
