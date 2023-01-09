3f224b2c3b6972aa4c75db93406ccee722d50dfb
Moch. Nafkhan Alzamzami <nafkhanalzamzami@gmail.com>
2023-01-09 09:21:16 +0700

    upgrade save_total_limit

--- mt5-pre-train/run.sh
+++ mt5-pre-train/run.sh
@@ -33,7 +33,7 @@ python -u run_multitask_unified_pretraining.py \
   --gradient_accumulation_steps $BATCH_SIZE \
   --model_type $MODEL \
   --model_name_or_path $MODEL \
-  --save_total_limit 2 \
+  --save_total_limit 10 \
   --do_train \
   --do_eval \
   --evaluate_during_training  \
